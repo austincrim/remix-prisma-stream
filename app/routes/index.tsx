e60301d92682b0730ebbae1cd77f244734958c53
Austin Crim <aust.crim@gmail.com>
2022-01-28 18:02:12 -0600

    WIP

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,27 @@
+import { PrismaClient } from '@prisma/client'
+import { json, LoaderFunction } from 'remix'
+
+const prisma = new PrismaClient()
+
+export let loader: LoaderFunction = async () => {
+  let results = await prisma.todo.findMany()
+
+  return json(results)
+}
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <form>
+        <label>
+          New Todo
+          <br />
+          <input type="text" />
+        </label>
+        <br />
+        <button type="submit">Add</button>
+      </form>
     </div>
-  );
+  )
 }

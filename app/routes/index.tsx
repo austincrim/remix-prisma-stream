918d0cff3bc12257c854688084b6d02bef192d82
Austin Crim <aust.crim@gmail.com>
2022-01-30 15:15:36 -0600

    form

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,27 +1,46 @@
-import { PrismaClient } from '@prisma/client'
-import { json, LoaderFunction } from 'remix'
-
-const prisma = new PrismaClient()
+import { prisma, PrismaClient, Todo } from '@prisma/client'
+import { ActionFunction, json, LoaderFunction, useLoaderData } from 'remix'
 
 export let loader: LoaderFunction = async () => {
+  const prisma = new PrismaClient()
   let results = await prisma.todo.findMany()
 
   return json(results)
 }
 
+export let action: ActionFunction = async ({ request }) => {
+  const prisma = new PrismaClient()
+  let title = (await request.formData()).get('title')?.toString()
+
+  await prisma.todo.create({
+    data: {
+      title,
+      completed: false
+    }
+  })
+
+  return json(null, { status: 200 })
+}
+
 export default function Index() {
+  let todos = useLoaderData<Todo[]>()
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>
-      <form>
+      <form method="POST" action="/?index">
         <label>
           New Todo
           <br />
-          <input type="text" />
+          <input name="title" type="text" required />
         </label>
         <br />
         <button type="submit">Add</button>
       </form>
+      <ul>
+        {todos.map((t) => (
+          <li key={t.id}>{t.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
